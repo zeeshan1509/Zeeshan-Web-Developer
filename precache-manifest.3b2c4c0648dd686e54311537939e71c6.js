babdb43e73aa8b3b7ad867085a2cc41e80075af2
zeeshan1509 <zeeshan1509@users.noreply.github.com>
2020-06-16 00:33:33 +0500

    Updates

---
+++ precache-manifest.3b2c4c0648dd686e54311537939e71c6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e0d5abd8d691d811b011e7e8a4c2fa",
+    "url": "/Zeeshan-Web-Developer/index.html"
+  },
+  {
+    "revision": "fe0375905b66af33ee00",
+    "url": "/Zeeshan-Web-Developer/static/css/2.dcb3bbc9.chunk.css"
+  },
+  {
+    "revision": "5898a0954cf01ef050c4",
+    "url": "/Zeeshan-Web-Developer/static/css/main.f903d5c7.chunk.css"
+  },
+  {
+    "revision": "fe0375905b66af33ee00",
+    "url": "/Zeeshan-Web-Developer/static/js/2.e64bb3fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Zeeshan-Web-Developer/static/js/2.e64bb3fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5898a0954cf01ef050c4",
+    "url": "/Zeeshan-Web-Developer/static/js/main.9887a903.chunk.js"
+  },
+  {
+    "revision": "bb55081beab640334a5c",
+    "url": "/Zeeshan-Web-Developer/static/js/runtime-main.77fe0a46.js"
+  },
+  {
+    "revision": "78e68b61b2b423e07e66e21d4938dc94",
+    "url": "/Zeeshan-Web-Developer/static/media/brain.78e68b61.png"
+  },
+  {
+    "revision": "2b69fde3ed2cbf5ac9c4731a52110b19",
+    "url": "/Zeeshan-Web-Developer/static/media/cad.2b69fde3.png"
+  },
+  {
+    "revision": "a32c44dd580d059e01534666cc7e6bc2",
+    "url": "/Zeeshan-Web-Developer/static/media/design.a32c44dd.jpg"
+  },
+  {
+    "revision": "4615bf75124ca82dc40ae3c07d59b82b",
+    "url": "/Zeeshan-Web-Developer/static/media/developer.4615bf75.png"
+  },
+  {
+    "revision": "00d62e6ff2aea445d3a4e70a64ea3083",
+    "url": "/Zeeshan-Web-Developer/static/media/js_logo.00d62e6f.png"
+  },
+  {
+    "revision": "f1f330057af169d9e725dae149d2ba75",
+    "url": "/Zeeshan-Web-Developer/static/media/logo_css.f1f33005.png"
+  },
+  {
+    "revision": "8ba1a66e461f2659c489cc912dcba64f",
+    "url": "/Zeeshan-Web-Developer/static/media/logo_html.8ba1a66e.png"
+  },
+  {
+    "revision": "4c6c0929cc58f241847a744b2de75e2f",
+    "url": "/Zeeshan-Web-Developer/static/media/logo_node.4c6c0929.png"
+  },
+  {
+    "revision": "8bac5bb613c05f8cfc6ba4570ca8453c",
+    "url": "/Zeeshan-Web-Developer/static/media/logo_postgres.8bac5bb6.png"
+  },
+  {
+    "revision": "70f87bba34487dc1c0cb8916d800c63b",
+    "url": "/Zeeshan-Web-Developer/static/media/logo_react.70f87bba.png"
+  },
+  {
+    "revision": "ec879c37d8eef4c1ac29f7e74543fad0",
+    "url": "/Zeeshan-Web-Developer/static/media/robo.ec879c37.png"
+  },
+  {
+    "revision": "ce0e61fd4ad769270b6d9686271175aa",
+    "url": "/Zeeshan-Web-Developer/static/media/showcase.ce0e61fd.jpg"
+  }
+]);
